76c486693f788431767148d0821e9c01db881c53
Trần Đức Tâm <kidnextdoor461993@gmail.com>
2026-09-10 08:55:31 +0700

    [Update] Export types for browser

--- src/browser/vanilla/index.ts
+++ src/browser/vanilla/index.ts
@@ -2,3 +2,8 @@ export * from "./constants";
 export * from "./providers";
 export * from "./types";
 export * from "./validators";
+
+export type * from "./constants";
+export type * from "./providers";
+export type * from "./types";
+export type * from "./validators";
